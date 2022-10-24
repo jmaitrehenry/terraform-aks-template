e3d081071eaeab35844838cbc8b30f28add9620c
Julien Maitrehenry <julien@kumojin.com>
2022-10-24 10:52:08 -0400

    Set load_config_file to false

--- main.tf
+++ main.tf
@@ -26,6 +26,7 @@ provider "kubectl" {
   client_certificate     = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config.0.client_certificate)
   client_key             = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config.0.client_key)
   cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config.0.cluster_ca_certificate)
+  load_config_file       = false
 }
 
 module "helm" {
